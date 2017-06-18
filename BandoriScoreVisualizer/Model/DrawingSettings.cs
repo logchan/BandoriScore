76a84f95025c02b98c4a822145c6e9bec42dcb9e
John Tan <johnmave126@gmail.com>
2017-06-18 11:35:25 +0800

    Slice the result output correctly Add more metadata display

--- BandoriScoreVisualizer/Model/DrawingSettings.cs
+++ BandoriScoreVisualizer/Model/DrawingSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace BandoriScoreVisualizer.Model
 {
@@ -7,7 +8,7 @@ namespace BandoriScoreVisualizer.Model
         public float NoteMargin { get; set; } = 48;
         public float NoteRadius { get; set; } = 20;
         public float BarHeight { get; set; } = 600;
-        public float ImageMargin { get; set; } = 100;
+        public float ImageMargin { get; set; } = 120;
         public int NumberOfBars { get; set; }
         public float NoteOuterWidth { get; set; } = 2;
         public float NoteRectWidth => NoteRadius * 2 + 6;
@@ -17,6 +18,13 @@ namespace BandoriScoreVisualizer.Model
         public float ImageHeight => ImageMargin * 2 + BarHeight * NumberOfBars;
 
         public float InnerRadius => NoteRadius * 0.8f;
+
+        public int BarChunkSize { get; set; } = 8;
+        public int NumberOfColumns => ((NumberOfBars - 1) / BarChunkSize) + 1;
+        public float FinalImageWidth => (ImageMargin + NoteMargin * 6) * NumberOfColumns + ImageMargin;
+        public float FinalImageHeight => ImageMargin * 2 + BarHeight * (Math.Min(NumberOfBars, BarChunkSize) + 1.0f / 4);
+        public float ColumnWidth => ImageWidth - ImageMargin;
+
 
         public PointF NotePosition(int track, Timing time)
         {
--- BandoriScoreVisualizer/Model/MetaData.cs
+++ BandoriScoreVisualizer/Model/MetaData.cs
@@ -2,7 +2,10 @@
 {
     public class MetaData
     {
-        public string Title { get; set; } = "NO TITLE";
+        public string Title { get; set; }
+        public string Difficulty { get; set; }
+        public int Level { get; set; }
+        public int Combo { get; set; }
         public int Bpm { get; set; }
     }
 }
